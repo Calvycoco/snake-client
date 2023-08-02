3e92a0c3807664a924ab18321e4961148cb47ec0
Calvycoco <su.calvin@live.com>
2023-08-02 16:58:26 -0700

    Snake game

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+const connect = () => {
+  console.log("Successfully connected to game server!")
+  const conn = net.createConnection({
+    host: "165.227.47.243",// IP address here,
+    port: 50541// PORT number here,
+  });
+  conn.setEncoding("utf8");
+  conn.write('Name: Cal')
+  conn.on("data", (data) => {
+    console.log("Received data from server:", data);
+  });
+
+  return conn;
+};
+module.exports = connect
--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: 50541// PORT number here,
-  });
-
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-  });
-
-  conn.on("data", (data) => {
-    console.log("Received data from server:", data);
-  });
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
