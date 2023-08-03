d001d33c7d321aac26b88a70bb3f3828f6597485
Calvycoco <su.calvin@live.com>
2023-08-02 17:59:57 -0700

    Snek revised

---
+++ input.js
@@ -0,0 +1,17 @@
+const { stdin } = require('process');
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function(input) {
+    if (input === '\u0003') {
+      process.exit();
+    }
+  };   
+    stdin.on("data", handleUserInput);
+    return stdin;
+};
+module.exports = { setupInput }
--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const connect = require('./client');
+const { setupInput } = require("./input");
 
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function(input) {
-    if (input === '\u0003') {
-      process.exit();
-    }
-  };
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 console.log("Connecting ...");
-connect();
-setupInput();
+connect()
+setupInput()
