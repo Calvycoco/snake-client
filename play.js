c315728184416a296e27b6b227f8999f2388088f
Calvycoco <su.calvin@live.com>
2023-08-02 17:30:31 -0700

    snek

--- play.js
+++ play.js
@@ -1,4 +1,19 @@
 const connect = require('./client');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function(input) {
+    if (input === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 console.log("Connecting ...");
 connect();
+setupInput();
